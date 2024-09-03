c91a23af9d663f996f89300878f80b3573c148b0
Akim Ospan <ospanakim05@gmail.com>
2024-09-04 04:37:27 +0500

    Home Component Finished

--- src/components/homeMenuComponents/index.jsx
+++ src/components/homeMenuComponents/index.jsx
@@ -1,11 +1,19 @@
 import NewTweet from "./newTweet/NewTweet";
 import TweetList from "./TweetLists/TweetLists";
+import './styles.css'
 
 export default function HomeMenu(){
     return(
         <div style={{width: '48%'}}>
-            {/* <h5>Home</h5> */}
-            {/* <NewTweet/> */}
+            <div className="d-flex justify-content-between align-items-center" style={{height: '5rem', borderBottom: '1px solid #e1e8ed'}}>
+                <div className="d-flex justify-content-center align-items-center hover  px-5 " style={{flex:1, height: '100%'}}>
+                    <a style={{fontWeight: 'bold', textDecoration: 'none', color: 'black'}}>For you</a>
+                </div>
+
+                <div className="d-flex justify-content-center align-items-center  hover  px-5" style={{flex:1,  height: '100%'}}>
+                    <p style={{fontWeight: 'bold', textDecoration: 'none', color: 'grey'}}>Following</p>
+                </div>
+            </div>
             <TweetList/>
         </div>
     )
