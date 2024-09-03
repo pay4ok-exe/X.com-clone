d11d41625892c891bfecb1abff88caa9c2a9506f
Akim Ospan <ospanakim05@gmail.com>
2024-09-04 01:08:33 +0500

    Side Menu Component Fully Finished

--- src/components/sideMenuComponents/MenuItem.jsx
+++ src/components/sideMenuComponents/MenuItem.jsx
@@ -1,10 +1,14 @@
+import './styles.css'
+
 export default function MenuItem({item}){
     return(
-        <div className='d-flex rounded-pill justify-content-start align-items-center'>
-            <div style={{width: 30, height: 30}}>
+        <div className='d-flex align-items-center menu-item'>
+            <div style={{width: 30, height: 30, marginLeft: '1rem'}}>
                 {item.icon}
             </div>
-            <p className='m-3' style={{fontSize: 20}}>{item.name}</p>
+            <p className='m-3 mt-2 mb-2' style={{fontSize: 20}}>
+                {item.name}
+            </p>
         </div>
     );
 }
--- src/components/sideMenuComponents/index.jsx
+++ src/components/sideMenuComponents/index.jsx
@@ -1,6 +1,7 @@
 import './styles.css'
-import { HOME_SVG, EXPLORE_SVG, NOTIFICATIONS_SVG, MESSAGES_SVG, BOOKMARKS_SVG } from './images'
+import { HOME_SVG, EXPLORE_SVG, NOTIFICATIONS_SVG, MESSAGES_SVG, BOOKMARKS_SVG, COMMUNITIES_SVG, PROFILE_SVG, MORE_SVG } from './images'
 import MenuItem from './menuItem';
+
 
 export default function SideMenu(){
     const menu = [
@@ -23,18 +24,43 @@ export default function SideMenu(){
         {
             icon: BOOKMARKS_SVG,
             name: 'Bookmarks'
+        },
+        {
+            icon: COMMUNITIES_SVG,
+            name: 'Communities'
+        },
+        {
+            icon: PROFILE_SVG,
+            name: 'Profile'
+        },
+        {
+            icon: MORE_SVG,
+            name: 'More'
         }
     ];
 
     return(
-        <div className='w-35 mt-3 mx-5 mb-2'>
-            <div className="logo">
+        <div className='w-35 mt-3 mx-5 mb-2 d-flex flex-column' style={{height:'100vh'}}>
+            <div className="logo mb-4">
                 <img src={require('./images/logo.svg')} className='logo-icon'></img>
             </div>
 
             {menu.map((item, index)=>(
-                <MenuItem key={index} item={item}/>
+                <MenuItem key={index} item={item} style={{gap: '20px'}}/>
             ))}
+
+            <button type="button" class="btn">Post</button>
+
+            <div className='profile'>
+                <img src={require('./images/profile.png')}/>
+                <div>
+                    <p id='pt1'>pay4ok</p>
+                    <p id='pt2'>@pay4ok_exe</p>
+                </div>
+                <a href='/' className='more_options'>
+                    <img src={require('./images/more_options.png')}/>
+                </a>
+            </div>
         </div>
         
     )
