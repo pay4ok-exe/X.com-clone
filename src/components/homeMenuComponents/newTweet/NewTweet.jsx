17d0f4b8d4d2aa20c50921d00f26fb61f0770e7e
Akim Ospan <ospanakim05@gmail.com>
2024-11-15 22:02:29 +0500

    Update

--- src/components/homeMenuComponents/newTweet/NewTweet.jsx
+++ src/components/homeMenuComponents/newTweet/NewTweet.jsx
@@ -1,29 +1,29 @@
 import { EARTH, MEDIA, GIF, POLL, EMOJI, SCHEDULE, LOCATION } from "../images";
-export default function NewTweet(){
-    return(
-        <div className="tweet">
-            <div className="tweet-avatar">
-                <img id='avatar' src={require('../images/profile.png')}/>
-            </div>
-            <div className="new-tweet-container">
-                <input placeholder="What is happening?!"/>
-                <div className="reply-container">
-                    {EARTH}
-                    <p>Everyone can reply</p>
-                </div>
-                <hr />
-                <div className="actions-container">
-                    <div className="symbols">
-                        {MEDIA}
-                        {GIF}
-                        {POLL}
-                        {EMOJI}
-                        {SCHEDULE}
-                        {LOCATION}
-                    </div>
-                    <button>Post</button>
-                </div>
-            </div>
+export default function NewTweet() {
+  return (
+    <div className="tweet">
+      <div className="tweet-avatar">
+        <img id="avatar" src={require("../images/profile.png")} />
+      </div>
+      <div className="new-tweet-container">
+        <input id="new-tweet-input" placeholder="What is happening?!" />
+        <div className="reply-container">
+          {/* {EARTH} */}
+          <p>Everyone can reply</p>
         </div>
-    )
+        <hr />
+        <div className="actions-container">
+          <div className="symbols">
+            {MEDIA}
+            {GIF}
+            {POLL}
+            {EMOJI}
+            {SCHEDULE}
+            {LOCATION}
+          </div>
+          <button>Post</button>
+        </div>
+      </div>
+    </div>
+  );
 }
