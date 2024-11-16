71dbe836d9c23d913cb204ee18fc8ac813c1c45b
Akim Ospan <ospanakim05@gmail.com>
2024-11-17 00:34:35 +0500

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import vitePluginRequire from "vite-plugin-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vitePluginRequire.default()],
-})
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+  },
+});
